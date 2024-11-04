5b98697200dee8d563ffa8afdcbe32d3275fa5dd
Ishasnath Gayan <ishanath.gayan@ancsis.com>
2024-11-04 23:44:41 +0530

    yaml changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const my_val = process.env.MY_VAL_STR;
+const my_val = process.env.MY_VAL;
 
 dataString = "hello world2";
 
